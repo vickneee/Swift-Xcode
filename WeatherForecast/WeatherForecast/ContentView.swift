2e994d46ab8b02260865c5b001ed0537f9db1d29
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-18 00:31:28 +0200

    Add styles

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -10,21 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast()
+            DayForecast(day: "Wed", isRainy: true, tHigh: 80, tLow: 50)
             
-            DayForecast()
+            DayForecast(day: "Thu", isRainy: false, tHigh: 70, tLow: 45)
         }
     }
 }
 
 struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let tHigh: Int
+    let tLow: Int
+    
+    var iconName: String {
+        isRainy ? "cloud.rain.fill" : "sun.max.fill"
+    }
+    
+    var iconColor: Color {
+        isRainy ? .blue : .yellow
+    }
+    
     var body: some View {
         VStack {
-            Text("Mon")
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("High: 70")
-            Text("Low: 50")
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("Hight: \(tHigh)")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(tLow)")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
         }
         .padding()
     }
